b30af8f7e6e22498b49daf2456496e3c16c40dcf
Beskargam <37930126+Beskargam@users.noreply.github.com>
2018-09-10 16:30:13 +0200

    Update newsView.php
    add security protocol include/require leak

--- Project5_Blog/View/newsView.php
+++ Project5_Blog/View/newsView.php
@@ -23,6 +23,26 @@ $title = htmlspecialchars($news->title());
             } ?></h4>
 
     </div>
-<?php $content = ob_get_clean(); ?>
+<?php $content = ob_get_clean();
 
-<?php require('..\View\Layout\layout.php'); ?>
+//require('..\View\Layout\layout.php');
+if (empty($pageLayout)) {
+    $pageLayout = 'layout';
+    $pageLayout = trim($pageLayout . '.php');
+}
+$pageLayout = str_replace('../', 'protect', $pageLayout);
+$pageLayout = str_replace('..\\', 'protect', $pageLayout);
+$pageLayout = str_replace(';', 'protect', $pageLayout);
+$pageLayout = str_replace('%', 'protect', $pageLayout);
+if (preg_match('/admin/', $pageLayout)) {
+    echo 'Vous n\'avez pas accès à ce répertoire';
+}
+else{
+    $pageLayout = '../View/Layout/'.$pageLayout;
+    if (file_exists($pageLayout) && $pageLayout != 'index.php') {
+        require($pageLayout);
+    }
+    else {
+        echo 'Page inexistante !';
+    }
+}
