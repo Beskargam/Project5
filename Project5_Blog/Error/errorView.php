b51d11cf2f864c134a5cac92b8aa41069cc8a770
Beskargam <37930126+Beskargam@users.noreply.github.com>
2018-09-11 17:47:21 +0200

    Update errorView.php

--- Project5_Blog/Error/errorView.php
+++ Project5_Blog/Error/errorView.php
@@ -7,7 +7,7 @@ $title = htmlspecialchars('Erreur');
     <h3><?php echo htmlspecialchars('' . $errorMessage) ?></h3>
 <?php $content = ob_get_clean();
 
-//require('..\View\Layout\layout.php');
+//require('View\Layout\layout.php');
 if (empty($pageLayout)) {
     $pageLayout = 'layout';
     $pageLayout = trim($pageLayout . '.php');
@@ -17,14 +17,14 @@ $pageLayout = str_replace('..\\', 'protect', $pageLayout);
 $pageLayout = str_replace(';', 'protect', $pageLayout);
 $pageLayout = str_replace('%', 'protect', $pageLayout);
 if (preg_match('/admin/', $pageLayout)) {
-    echo 'Vous n\'avez pas accès à ce répertoire';
+    throw new Exception('Cette zone est réservée au personnel abilité uniquement');
 }
 else{
-    $pageLayout = '../View/Layout/'.$pageLayout;
+    $pageLayout = 'View/Layout/'.$pageLayout;
     if (file_exists($pageLayout) && $pageLayout != 'index.php') {
         require($pageLayout);
     }
     else {
-        echo 'Page inexistante !';
+        throw new Exception('Vous naviguez dans l\'espace inconnu, il n\'y a rien dans cette zone...');
     }
 }
