ad6c13c9b226ea8018c93af222dcfa32bedd98c6
Beskargam <37930126+Beskargam@users.noreply.github.com>
2018-09-19 11:01:18 +0200

    Add User class

---
+++ Project5_Blog/Model/Entity/User.php
@@ -0,0 +1,71 @@
+<?php
+
+class User
+{
+    private $id;
+    private $pseudo;
+    private $rank;
+    private $password;
+
+// CONSTRUCTOR
+    public function __construct($value = [])
+    {
+        if (!empty($value)) {
+            $this->hydrate($data);
+        }
+
+    }
+
+    public function hydrate($data)
+    {
+        foreach ($data as $key => $value) {
+            $method = 'set' . ucfirst($key);
+
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+
+// GETTERS
+    public function id()
+    {
+        return $this->id;
+    }
+
+    public function pseudo()
+    {
+        return $this->pseudo;
+    }
+
+    public function rank()
+    {
+        return $this->rank;
+    }
+
+    public function password()
+    {
+        return $this->password;
+    }
+
+// SETTERS
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setPseudo($pseudo)
+    {
+        $this->$pseudo = $pseudo;
+    }
+
+    public function setRank($rank)
+    {
+        $this->rank = $rank;
+    }
+
+    public function setPassword($password)
+    {
+        $this->$password = $password;
+    }
+}
