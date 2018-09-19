0d140a21d02a344cfab8718447448f8dc4d498a5
Beskargam <37930126+Beskargam@users.noreply.github.com>
2018-09-19 11:00:14 +0200

    add files via upload
    logout.php
    UsersManager.php
    UsersManagerPDO.php

---
+++ Project5_Blog/Model/UsersManagerPDO.php
@@ -0,0 +1,34 @@
+<?php
+
+class UsersManagerPDO extends UsersManager
+{
+    protected $db;
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function getConnexionUser()
+    {
+
+        $pseudo = $_POST['pseudo'];
+        $password = $_POST['password'];
+
+        $request = $this->db->prepare(
+            'SELECT * 
+            FROM users u
+			INNER JOIN ranks r
+			ON u.rank_id = r.id 
+            WHERE pseudo = :pseudo AND password = :password');
+        $request->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
+        $request->bindValue(':password', $password, PDO::PARAM_STR);
+        $request->execute();
+
+        $request->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'user');
+
+        $user = $request->fetch();
+        return $user;
+    }
+
+}
