a039e8a541d075cc79b793d8e4d1f0bf4093dd5c
Beskargam <37930126+Beskargam@users.noreply.github.com>
2018-09-19 10:48:45 +0200

    Update DataBase
    Change author name to pseudo name

--- Project5_Blog/project5_db.sql
+++ Project5_Blog/project5_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 04 sep. 2018 à 13:19
+-- Généré le :  mer. 19 sep. 2018 à 08:36
 -- Version du serveur :  5.7.19
 -- Version de PHP :  7.1.9
 
@@ -32,12 +32,12 @@ DROP TABLE IF EXISTS `comments`;
 CREATE TABLE IF NOT EXISTS `comments` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `news_id` int(10) UNSIGNED NOT NULL,
-  `author_id` int(10) UNSIGNED NOT NULL,
+  `pseudo_id` int(10) UNSIGNED NOT NULL,
   `content_comment` text NOT NULL,
   `dateAdd_comment` datetime NOT NULL,
   `dateEdit_comment` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_author_id` (`author_id`) USING BTREE,
+  KEY `fk_author_id` (`pseudo_id`) USING BTREE,
   KEY `fk_news_id` (`news_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -50,20 +50,20 @@ CREATE TABLE IF NOT EXISTS `comments` (
 DROP TABLE IF EXISTS `news`;
 CREATE TABLE IF NOT EXISTS `news` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `author_id` int(10) UNSIGNED NOT NULL,
+  `pseudo_id` int(10) UNSIGNED NOT NULL,
   `title` varchar(100) NOT NULL,
   `content_news` text NOT NULL,
   `dateAdd_news` datetime NOT NULL,
   `dateEdit_news` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_user_id` (`author_id`)
+  KEY `fk_user_id` (`pseudo_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `news`
 --
 
-INSERT INTO `news` (`id`, `author_id`, `title`, `content_news`, `dateAdd_news`, `dateEdit_news`) VALUES
+INSERT INTO `news` (`id`, `pseudo_id`, `title`, `content_news`, `dateAdd_news`, `dateEdit_news`) VALUES
 (1, 1, 'TITRE TEST', 'TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE \r\nTEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE \r\nTEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE \r\n\r\nTEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE TEXTE ', '2018-08-17 10:30:00', '2018-08-17 10:30:00'),
 (2, 1, 'Deuxième titre News', 'CECI EST UN TEXT D\'EXEMPLE CECI EST UN TEXT D\'EXEMPLE CECI EST UN TEXT D\'EXEMPLE CECI EST UN TEXT D\'EXEMPLE CECI EST UN TEXT D\'EXEMPLE CECI EST UN TEXT D\'EXEMPLE CECI EST UN TEXT D\'EXEMPLE CECI EST UN TEXT D\'EXEMPLE CECI EST UN TEXT D\'EXEMPLE \r\n\r\nCECI EST UN TEXT D\'EXEMPLE \r\nCECI EST UN TEXT D\'EXEMPLE CECI EST UN TEXT D\'EXEMPLE \r\nCECI EST UN TEXT D\'EXEMPLE CECI EST UN TEXT D\'EXEMPLE CECI EST UN TEXT D\'EXEMPLE \r\nCECI EST UN TEXT D\'EXEMPLE CECI EST UN TEXT D\'EXEMPLE ', '2018-08-24 11:23:19', '2018-09-04 15:01:52');
 
@@ -99,11 +99,11 @@ INSERT INTO `ranks` (`id`, `rank`) VALUES
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `author` varchar(50) NOT NULL,
-  `password` varchar(30) NOT NULL,
+  `pseudo` varchar(50) NOT NULL,
+  `password` char(255) NOT NULL,
   `rank_id` smallint(5) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `author` (`author`),
+  UNIQUE KEY `author` (`pseudo`),
   KEY `fk_rank_id` (`rank_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
@@ -111,7 +111,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`id`, `author`, `password`, `rank_id`) VALUES
+INSERT INTO `users` (`id`, `pseudo`, `password`, `rank_id`) VALUES
 (1, 'Arcturus Morea', 'NdCHv1417', 1);
 
 --
@@ -122,14 +122,14 @@ INSERT INTO `users` (`id`, `author`, `password`, `rank_id`) VALUES
 -- Contraintes pour la table `comments`
 --
 ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`pseudo_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `fk_news_id` FOREIGN KEY (`news_id`) REFERENCES `news` (`id`) ON DELETE CASCADE;
 
 --
 -- Contraintes pour la table `news`
 --
 ALTER TABLE `news`
-  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`pseudo_id`) REFERENCES `users` (`id`);
 
 --
 -- Contraintes pour la table `users`
