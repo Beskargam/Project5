6c47ada15d864da4b1b3f03810510220a95b8c5e
Beskargam <37930126+Beskargam@users.noreply.github.com>
2018-09-21 15:21:59 +0200

    Update newsView.php

--- Project5_Blog/View/Frontend/newsView.php
+++ Project5_Blog/View/Frontend/newsView.php
@@ -3,10 +3,8 @@ $title = htmlspecialchars($news->title());
 ?>
 
 <?php ob_start(); ?>
-    <p><a href="index.php?action=home">Retour à la liste des billets</a></p>
-
     <section class="news">
-
+        <p><a href="index.php?action=home">Retour à la liste des billets</a></p>
         <h3><?php echo htmlspecialchars($news->title()) ?></h3>
 
         <p><?php
@@ -21,8 +19,42 @@ $title = htmlspecialchars($news->title());
                 ?> - Modifié le
                 <em><?php echo htmlspecialchars($news->dateEdit_news()->format('d-m-Y H:i')) ?></em><?php
             } ?></h4>
-
     </section>
+    <section class="comments">
+        <?php
+        foreach ($commentsList as $comment) { ?>
+            <hr>
+            <h4> <?php
+                echo htmlspecialchars($comment->rank_user()) ?><br><?php
+                echo htmlspecialchars($comment->pseudo()) ?><br>
+            </h4>
+            <p><?php
+                echo nl2br(htmlspecialchars($comment->content_comment()))
+                ?></p>
+            <h4>Posté le <em><?php echo htmlspecialchars($comment->dateAdd_comment()->format('d-m-Y H:i')) ?></em>
+            <?php
+            if ($comment->dateAdd_comment() != $comment->dateEdit_comment()) {
+                ?> - Modifié le
+                <em><?php echo htmlspecialchars($comment->dateEdit_comment()->format('d-m-Y H:i')) ?></em><?php
+            } ?>
+            </h4>
+            <hr><?php
+        } ?>
+    </section>
+<?php
+if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']) AND isset($_SESSION['rank'])) {
+    echo '<section class="addComment">
+        <form action="index.php?action=news&amp;id=' . htmlspecialchars($news->id_news()) . '" method="post">
+            <label for="addCommentary">Commentaire : </label> <textarea name="addCommentary"
+                                                                      id="addCommentary" required></textarea>
+            <input type="submit" value="Envoyer">
+        </form>
+    </section>';
+}
+else {
+    echo 'Si vous souhaitez réagir à cette news, il faut vous inscrire ou vous connecter';
+}
+?>
 <?php $content = ob_get_clean();
 
 //require('View\Layout\layout.php');
