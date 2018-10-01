a22ab7d3796f6b629dacad91e47e7e677173752b
Beskargam <37930126+Beskargam@users.noreply.github.com>
2018-10-01 18:19:23 +0200

    Update CommentsManager.php

--- Project5_Blog/Model/CommentsManager.php
+++ Project5_Blog/Model/CommentsManager.php
@@ -4,11 +4,5 @@ abstract class CommentsManager
 {
     abstract public function getValidListComments($id);
 
-    abstract public function getListComments($id);
-
-    abstract protected function addComments($id, $content_comment);
-
-    abstract public function deleteComments($id_comment);
-
-    abstract protected function updateComments(comment $id_comment, $NewContent_comment);
+    abstract public function addComment($id, $content_comment);
 }
