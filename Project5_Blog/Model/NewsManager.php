ba88763e36be2fed002ccbe894f79dc0aad89a7c
Beskargam <37930126+Beskargam@users.noreply.github.com>
2018-10-01 18:20:55 +0200

    Update NewsManager.php

--- Project5_Blog/Model/NewsManager.php
+++ Project5_Blog/Model/NewsManager.php
@@ -5,10 +5,4 @@ abstract class NewsManager
     abstract public function getListNews();
 
     abstract public function getNews($id);
-
-    abstract protected function addNews(News $news);
-
-    abstract public function deleteNews($id);
-
-    abstract protected function updateNews(News $news);
 }
