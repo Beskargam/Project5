be17b02eb6c7e94667a9e6fc0c71cfa718a93b7c
Beskargam <37930126+Beskargam@users.noreply.github.com>
2018-10-18 17:56:33 +0200

    Update backFooter.php

--- Project5_Blog/Backend/BackView/backFooter.php
+++ Project5_Blog/Backend/BackView/backFooter.php
@@ -4,10 +4,27 @@ if (!isset($_SESSION)) {
 }
 
 ob_start(); ?>
-    <footer>
-        <div>
-            <hr>
-            <a href="backIndex.php">Home</a>
+    <footer class="footer-text">
+        <div class="row justify-content-center">
+            <div class="footer-col">
+                <div class="footer-title">
+                    <a class="footer-logo" href="index.php?action=home"><img src="BackPublic/Images/logo.png" width="64"
+                                                                             height="30" alt=""></a>|
+                    <span class="footer-title-text">NAVIGATION</span>
+                </div>
+                <ul class="footer-nav">
+                    <li class="footer-nav-item"><a class="footer-link" href="backIndex">HOME</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </footer>
+    <footer class="bottom-footer">
+        <div class="row justify-content-center">
+            <div class="col-sm-12">
+                @ 2018 Arcturus Morea Transport Intergalactic Corp.
+            </div>
+        </div>
+    </footer>
+    <footer>
 <?php $footer = ob_get_clean();
