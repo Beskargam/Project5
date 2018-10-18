24500cc7d69a99f9119a080390ffd4a3d4dd964f
Beskargam <37930126+Beskargam@users.noreply.github.com>
2018-10-18 17:55:00 +0200

    Update backUsersListView.php

--- Project5_Blog/Backend/BackView/backUsersListView.php
+++ Project5_Blog/Backend/BackView/backUsersListView.php
@@ -3,20 +3,49 @@ $title = htmlspecialchars('Liste des Membres');
 
 ob_start(); ?>
     <section class="usersList">
-        <p>Voici la liste des membres : </p>
-        <table>
-            <tr>
-                <th>Rang</th>
-                <th>Nom</th>
-            </tr>
-            <?php
-            foreach ($usersList as $users) { ?>
-                <tr>
-                    <td><?php echo htmlspecialchars($users->rank()) . ' '; ?></td>
-                    <td><?php echo htmlspecialchars($users->pseudo()); ?></td>
-                </tr>
-            <?php } ?>
-        </table>
+        <div class="container">
+            <div class="row title-home">
+                <div class="col-sm-12 title-home-text">
+                    MEMBRES /
+                </div>
+            </div>
+            <div class="container flipboard-boxes flipboard-latest-news">
+                <div class="row">
+                    <div class="col-lg-12 card-inscription">
+                        <div class="card-inscription-body">
+
+                            <div id="upper_left-corner"></div>
+                            <div id="upper_right-corner"></div>
+                            <div id="lower_left-corner"></div>
+                            <div id="lower_right-corner"></div>
+
+                            <h4 class="card-inscription-subtitle">TYPE / <span class="type-group">EQUIPAGE /</span>
+                            </h4>
+                            <h3 class="card-inscription-title">Liste des Membres</h3>
+                            <p class="card-inscription-text">
+                            <table class="table table-striped table-dark">
+                                <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col">Rang</th>
+                                    <th scope="col">Nom</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                foreach ($usersList as $users) { ?>
+                                    <tr>
+                                        <td class=""><?php echo htmlspecialchars($users->rank()) . ' '; ?></td>
+                                        <td class=""><?php echo htmlspecialchars($users->pseudo()); ?></td>
+                                    </tr>
+                                <?php } ?>
+                                </tbody>
+                            </table>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 <?php $content = ob_get_clean();
 
