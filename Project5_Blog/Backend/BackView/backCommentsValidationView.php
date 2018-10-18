4834415b63105a151137ad61cb33207d0f0eab38
Beskargam <37930126+Beskargam@users.noreply.github.com>
2018-10-18 17:59:01 +0200

    Update backCommentsValidationView.php

--- Project5_Blog/Backend/BackView/backCommentsValidationView.php
+++ Project5_Blog/Backend/BackView/backCommentsValidationView.php
@@ -4,31 +4,71 @@ $title = htmlspecialchars('Validation des Commentaires');
 
 <?php ob_start(); ?>
     <section class="comments">
+        <div class="container">
+            <div class="col-lg-12 flipboard-boxes flipboard-latest-news">
+                <div class="row justify-content-center">
+                    <div class="card-comment-validation">
+                        <img class="card-img" src="BackPublic/Images/Source.jpg" alt="Card-image">
+                        <div class="card-latestnews-body card-img-overlay">
+
+                            <div id="upper_left-corner"></div>
+                            <div id="upper_right-corner"></div>
+                            <div id="lower_left-corner"></div>
+                            <div id="lower_right-corner"></div>
+
+                            <h4 class="card-latestnews-subtitle">TYPE / <span class="type-group">VALIDATION /</span></h4>
+                            <h3 class="card-latestnews-title">VALIDATION DES COMMENTAIRES</h3>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="two-line-separator"></div>
         <?php
         foreach ($commentsValidationList as $comment) { ?>
-            <hr>
-            <h4> <?php
-                echo htmlspecialchars($comment->rank_user()) ?><br><?php
-                echo htmlspecialchars($comment->pseudo()) ?><br>
-            </h4>
-            <p><?php
-                echo nl2br(htmlspecialchars($comment->content_comment()))
-                ?></p>
-            <h4>Posté le <em><?php echo htmlspecialchars($comment->dateAdd_comment()->format('d-m-Y H:i')) ?></em>
-                <?php
-                if ($comment->dateAdd_comment() != $comment->dateEdit_comment()) {
-                    ?> - Modifié le
-                    <em><?php echo htmlspecialchars($comment->dateEdit_comment()->format('d-m-Y H:i')) ?></em><?php
-                } ?>
-            </h4>
-            <form action="backIndex.php?action=commentsValidationList" method="POST">
-                <?php
-                echo '<input type="hidden" name="id" value="' .htmlspecialchars($comment->id_comment()). '">'
-                ?>
-                <button type="submit" name="validateComment" value="validate">Valider le commentaire</button>
-            </form>
-            <hr><?php
-        } ?>
+            <div class="container flipboard-boxes flipboard-comment">
+                <div class="row">
+                    <div class="col-lg-12 card-comment">
+                        <div class="card-comment-body">
+
+                            <div class="card-comment-header">
+                                <div class="row">
+                                    <div class="col-lg-9">
+                                        <h4 class="comment-pseudo"><?php echo htmlspecialchars($comment->rank_user()); ?>
+                                            /
+                                            <span class="comment-name"><?php echo htmlspecialchars($comment->pseudo()); ?>
+                                                /</span>
+                                        </h4>
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <h4 class="date-comment">
+                                            <?php
+                                            if ($comment->dateAdd_comment() != $comment->dateEdit_comment()) {
+                                                ?> [MAJ] <?php echo htmlspecialchars($comment->dateEdit_comment()->format('d F, Y H:i')) ?>
+                                            <?php } else {
+                                                echo htmlspecialchars($comment->dateAdd_comment()->format('d F, Y H:i'));
+                                            } ?>
+                                        </h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="card-comment-text">
+                                <?php echo nl2br(htmlspecialchars($comment->content_comment())); ?>
+                            </p>
+                            <div class="card-comment-footer">
+                                <form action="backIndex.php?action=commentsValidationList" method="POST">
+                                    <?php
+                                    echo '<input type="hidden" name="id" value="' .htmlspecialchars($comment->id_comment()). '">'
+                                    ?>
+                                    <button class="btn btn-primary" type="submit" name="validateComment" value="validate">Valider le commentaire</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
     </section>
 <?php $content = ob_get_clean();
 
