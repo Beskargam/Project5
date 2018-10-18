5269b1d5eed7fafd057f0cb7a80963be442849b4
Beskargam <37930126+Beskargam@users.noreply.github.com>
2018-10-18 17:39:07 +0200

    Update layout.php

--- Project5_Blog/View/Layout/layout.php
+++ Project5_Blog/View/Layout/layout.php
@@ -1,14 +1,24 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr" xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="Public/Css/bootstrap.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Electrolize" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
     <title><?= $title ?></title>
-    <link href="style.css" rel="stylesheet"/>
 </head>
-<body>
+<body class="page-bg">
 <?= $header ?>
 <?= $navigation ?>
 <?= $content ?>
 <?= $footer ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+<script src="Public/Js/bootstrap.bundle.js"></script>
+<script src="Public/Js/bootstrap.js"></script>
 </body>
 </html>
